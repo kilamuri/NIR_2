be5b061e6f027ca32e359fdf9573d3761e84acb0
Екатерина Кантемирова <kilamuri@gmail.com>
2022-12-15 00:38:26 +0300

    Сделала работу с вебкамерой.

--- program_form.py
+++ program_form.py
@@ -8,8 +8,12 @@ import numpy as np
 def open_img(path_img):
     pil_img = Image.open(path_img)
     width, height = pil_img.size
-    new_height  = 550 
-    new_width = int(new_height * width / height)
+    if height > width:
+        new_height  = 550 
+        new_width = int(new_height * width / height)
+    else:
+        new_width  = 500 
+        new_height = int(new_width * height / width)
 
     pil_img = pil_img.resize((new_width, new_height))
     test = ImageTk.PhotoImage(pil_img)
@@ -43,6 +47,29 @@ def eye_detect():
 
     cv2.imwrite("result.jpg", frame)
     open_img("result.jpg")
+    
+def camera():
+    cap = cv2.VideoCapture(0)
+    path = "haarcascade_eye.xml"
+    eyeCascade = cv2.CascadeClassifier(path)
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        eyes = eyeCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(35, 35))
+
+        if len(eyes) > 0:
+            for (x, y, w, h) in eyes:
+                frame[y+10:y+h-10, x:x+w, 0] = np.random.normal(size=(h-20, w))
+                frame[y+10:y+h-10, x:x+w, 1] = np.random.normal(size=(h-20, w))
+                frame[y+10:y+h-10, x:x+w, 2] = np.random.normal(size=(h-20, w))
+            
+        cv2.imshow('Detect eyes', frame)
+        if cv2.waitKey(10) == 27: # Клавиша Esc
+            break
+    
+    cap.release()    
+    cv2.destroyAllWindows()
 
 # дизайн формы
 root = Tk()
@@ -51,9 +78,11 @@ root.geometry('800x600')
 
 font_button = font.Font(size=14, weight="normal")
 
-btn1 = Button(root, width = 20,text = "Открыть фотку", command = open_file, font = font_button)
-btn2 = Button(root, width = 20,text = "Сделать хуету", command = eye_detect, font = font_button)
-btn1.place(x = 500, y = 25)
-btn2.place(x = 500, y = 80)
+btn1 = Button(root, width = 20,text = "Открыть фотографию", command = open_file, font = font_button)
+btn2 = Button(root, width = 20,text = "Закрасить глаза\nна фотографии", command = eye_detect, font = font_button)
+btn3 = Button(root, width = 20,text = "Открыть веб-камеру\nи закрасить глаза", command = camera, font = font_button)
+btn1.place(x = 550, y = 25)
+btn2.place(x = 550, y = 80)
+btn3.place(x = 550, y = 155)
 
 root.mainloop() 
